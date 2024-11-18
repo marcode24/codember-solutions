0b78672b41d192f6ba80e7037c666baefac7b27b
Marco Cruz <marco24cruz08@gmail.com>
2024-11-18 15:20:59 -0600

    ✅ Fix test for challenge-01

--- 2022/01-arregla-twitter/solution.js
+++ 2022/01-arregla-twitter/solution.js
@@ -1,19 +1,34 @@
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable operator-linebreak */
 const fs = require('fs');
 
 const obtenerUsuariosIncorrectos = (fileName) => {
   let results = [];
   try {
-    const text = fs.readFileSync(`./2022/01-arregla-twitter/${fileName}`, 'utf8');
-    const arr = text.toString().split(/\n\s*\n/).map((el) => el.replace(/\n/g, ' '));
-    results = arr.filter((data) => data.includes('usr:')
-            && data.includes('eme:')
-            && data.includes('psw:')
-            && data.includes('age:')
-            && data.includes('loc:')
-            && data.includes('fll:'));
-    return `submit: ${results.length}${results.at(-1)}`;
+    const text = fs.readFileSync(
+      `./2022/01-arregla-twitter/${fileName}`,
+      'utf8',
+    );
+    const arr = text
+      .toString()
+      .split(/\n\s*\n/)
+      .map((el) => el.replace(/\n/g, ' '));
+    results = arr.filter(
+      (data) =>
+        data.includes('usr:') &&
+        data.includes('eme:') &&
+        data.includes('psw:') &&
+        data.includes('age:') &&
+        data.includes('loc:') &&
+        data.includes('fll:'),
+    );
+    return `submit: ${results.length}${results
+      .at(-1)
+      .replace(/\s/g, ' ')
+      .replace(/\s{2,}/g, ' ')
+      .trim()}`;
   } catch (e) {
-    return `Error: ${e.message}`;
+    return `Error: when reading file ${fileName}`;
   }
 };
 
